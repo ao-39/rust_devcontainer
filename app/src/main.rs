2b9f92af8dc2b0332a8df93e08a5152b42b1dba9
ao-39 <fukahireee@outlook.jp>
2023-08-06 12:34:43 +0900

    feat: main.rsにロガー設定を移動させた

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,12 @@
-mod common;
+use tracing_subscriber::{fmt::time::LocalTime, EnvFilter};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv()?;
-    common::logger::init();
+    tracing_subscriber::fmt()
+        .with_timer(LocalTime::rfc_3339())
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_file(true)
+        .with_line_number(true)
+        .init();
     tracing::info!("Hello, world!");
     Ok(())
 }
