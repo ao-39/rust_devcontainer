10a7b98d4ad4d7c068487681b94709acfaf29252
ao-39 <fukahireee@outlook.jp>
2023-08-11 02:02:27 +0900

    feat: openapiを追加した

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,46 @@
 mod common;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+use axum::{routing::get, Router};
+
+use utoipa::OpenApi;
+use utoipa_rapidoc::RapiDoc;
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(health),
+        tags(
+            (name = "APIドキュメント", description = "APIドキュメントです。")
+        )
+    )]
+    struct ApiDoc;
+
     dotenvy::dotenv()?;
     common::logger::init();
-    tracing::info!("Hello, world!");
+    tracing::info!("アプリケーションが起動しました");
+    let app = Router::new()
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
+        .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+        .route("/helth", get(health));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
     Ok(())
+}
+
+#[utoipa::path(
+    get,
+    path = "/helth",
+    responses(
+        (status = 200, description = "ヘルスチェック")
+    )
+)]
+async fn health() -> &'static str {
+    "OK"
 }
