ffebbe14604d64a3d00aa29390940ec1551a5039
ao-39 <fukahireee@outlook.jp>
2023-08-26 14:47:16 +0900

    feat: ユーザー識別子のバリデーションを値オブジェクトに追加した

--- app/domain/examples/create_user.rs
+++ app/domain/examples/create_user.rs
@@ -4,7 +4,7 @@ use tracing::info;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let user = domain::entity::User::new(
         rusty_ulid::Ulid::generate(),
-        domain::object::UserDiscriminator::new("john".to_string()),
+        domain::object::UserDiscriminator::new("john".to_string())?,
         domain::object::UserName::new("John Doe".to_string()),
         email_address::EmailAddress::from_str("example@example.com")?,
         url::Url::parse("https://example.com")?,
--- app/domain/src/object/user_discriminator.rs
+++ app/domain/src/object/user_discriminator.rs
@@ -1,10 +1,16 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct UserDiscriminator(String);
 
 impl UserDiscriminator {
-    pub fn new(discriminator: String) -> Self {
-        Self(discriminator)
+    pub fn new(discriminator: String) -> Result<Self, String> {
+        static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-zA-Z][a-zA-Z0-9-]{2,23}$").unwrap());
+        if RE.is_match(&discriminator) {
+            return Err("バリデーションエラー".to_string());
+        };
+        Ok(Self(discriminator))
     }
 }
