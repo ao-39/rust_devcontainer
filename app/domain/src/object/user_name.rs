88bf744832e3d1a9f035b02abe7b62ed22afd9ca
ao-39 <fukahireee@outlook.jp>
2023-08-26 14:50:40 +0900

    feat: ユーザー名値オブジェクトにバリデーションを追加した

--- app/domain/src/object/user_name.rs
+++ app/domain/src/object/user_name.rs
@@ -4,7 +4,11 @@ use serde::{Deserialize, Serialize};
 pub struct UserName(String);
 
 impl UserName {
-    pub fn new(name: String) -> Self {
-        Self(name)
+    pub fn new(name: String) -> Result<Self, String> {
+        if name.len() >= 3 && name.len() <= 80 {
+            Ok(Self(name))
+        } else {
+            return Err("バリデーションエラー".to_string());
+        }
     }
 }
