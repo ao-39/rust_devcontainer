d9c5684420f555594aab7ce02deb2b646dc3f36f
ao-39 <fukahireee@outlook.jp>
2023-09-03 16:28:26 +0900

    :sparkles: ユーザーアプリケーションサービスを実装した

--- app/app_service/app_service/src/user.rs
+++ app/app_service/app_service/src/user.rs
@@ -1,5 +1,8 @@
 use async_trait::async_trait;
-use domain::object::{email_address::EmailAddress, url::Url, UserDiscriminator, UserName};
+use domain::{
+    entity::User,
+    object::{email_address::EmailAddress, url::Url, UserDiscriminator, UserName},
+};
 
 #[async_trait]
 pub trait IUserAppService {
@@ -10,10 +13,49 @@ pub trait IUserAppService {
         email: EmailAddress,
         web_page: Option<Url>,
     ) -> Result<(), UserRegisterError>;
+
+    async fn find_by_discriminator(
+        &self,
+        discriminator: UserDiscriminator,
+    ) -> Result<User, UserFindError>;
+
+    async fn find_by_email(&self, email: EmailAddress) -> Result<User, UserFindError>;
+
+    async fn delete(&self, discriminator: UserDiscriminator) -> Result<(), UserDeleteError>;
+
+    async fn update(
+        &self,
+        discriminator: UserDiscriminator,
+        update_operator: UserUpdateOperator,
+    ) -> Result<(), UserUpdateError>;
 }
 
 pub enum UserRegisterError {
     DuplicateDiscriminator,
     DuplicateEmail,
     OtherError,
+}
+
+pub enum UserFindError {
+    NotFound,
+    OtherError,
+}
+
+pub enum UserDeleteError {
+    NotFound,
+    OtherError,
+}
+
+pub enum UserUpdateError {
+    NotFound,
+    DuplicateDiscriminator,
+    DuplicateEmail,
+    OtherError,
+}
+
+pub enum UserUpdateOperator {
+    Discriminator(UserDiscriminator),
+    Name(UserName),
+    Email(EmailAddress),
+    WebPage(Option<Url>),
 }
--- app/app_service/app_service_impl/src/user/register.rs
+++ app/app_service/app_service_impl/src/user/register.rs
@@ -1,4 +1,7 @@
-use app_service::user::{IUserAppService, UserRegisterError};
+use app_service::user::{
+    IUserAppService, UserDeleteError, UserFindError, UserRegisterError, UserUpdateError,
+    UserUpdateOperator,
+};
 use async_trait::async_trait;
 use domain::{
     entity::User,
@@ -6,7 +9,10 @@ use domain::{
         chrono::Local, email_address::EmailAddress, rusty_ulid::Ulid, url::Url, UserDiscriminator,
         UserName,
     },
-    repository::IUserRepository,
+    repository::{
+        IUserRepository, UserRepositoryAddError, UserRepositoryDeleteError,
+        UserRepositoryFindError, UserRepositoryUpdateError,
+    },
 };
 
 pub struct UserApplicationService<T: IUserRepository + Sync + Send> {
@@ -38,13 +44,97 @@ impl<T: IUserRepository + Sync + Send> IUserAppService for UserApplicationServic
 
         match res {
             Err(err) => match err {
-                domain::repository::UserRepositoryAddError::DuplicateDiscriminator => {
+                UserRepositoryAddError::DuplicateDiscriminator => {
                     Err(UserRegisterError::DuplicateDiscriminator)
                 }
-                domain::repository::UserRepositoryAddError::DuplicateEmail => {
-                    Err(UserRegisterError::DuplicateEmail)
-                }
+                UserRepositoryAddError::DuplicateEmail => Err(UserRegisterError::DuplicateEmail),
                 _ => Err(UserRegisterError::OtherError),
+            },
+            Ok(_) => Ok(()),
+        }
+    }
+
+    async fn find_by_discriminator(
+        &self,
+        discriminator: UserDiscriminator,
+    ) -> Result<User, UserFindError> {
+        let res = self
+            .user_repository
+            .find_by_discriminator(discriminator)
+            .await;
+
+        match res {
+            Err(err) => match err {
+                UserRepositoryFindError::NotFound => Err(UserFindError::NotFound),
+                _ => Err(UserFindError::OtherError),
+            },
+            Ok(user) => Ok(user),
+        }
+    }
+
+    async fn find_by_email(&self, email: EmailAddress) -> Result<User, UserFindError> {
+        let res = self.user_repository.find_by_email(email).await;
+
+        match res {
+            Err(err) => match err {
+                UserRepositoryFindError::NotFound => Err(UserFindError::NotFound),
+                _ => Err(UserFindError::OtherError),
+            },
+            Ok(user) => Ok(user),
+        }
+    }
+
+    async fn delete(&self, discriminator: UserDiscriminator) -> Result<(), UserDeleteError> {
+        let res = self.user_repository.delete(discriminator).await;
+
+        match res {
+            Err(err) => match err {
+                UserRepositoryDeleteError::NotFound => Err(UserDeleteError::NotFound),
+                _ => Err(UserDeleteError::OtherError),
+            },
+            Ok(_) => Ok(()),
+        }
+    }
+
+    async fn update(
+        &self,
+        discriminator: UserDiscriminator,
+        update_operator: UserUpdateOperator,
+    ) -> Result<(), UserUpdateError> {
+        let mut user = self
+            .user_repository
+            .find_by_discriminator(discriminator)
+            .await
+            .map_err(|err| match err {
+                UserRepositoryFindError::NotFound => UserUpdateError::NotFound,
+                _ => UserUpdateError::OtherError,
+            })?;
+
+        match update_operator {
+            UserUpdateOperator::Discriminator(discriminator) => {
+                user.discriminator = discriminator;
+            }
+            UserUpdateOperator::Name(name) => {
+                user.name = name;
+            }
+            UserUpdateOperator::Email(email) => {
+                user.email = email;
+            }
+            UserUpdateOperator::WebPage(web_page) => {
+                user.web_page = web_page;
+            }
+        }
+
+        let res = self.user_repository.update(user).await;
+
+        match res {
+            Err(err) => match err {
+                UserRepositoryUpdateError::NotFound => Err(UserUpdateError::NotFound),
+                UserRepositoryUpdateError::DuplicateDiscriminator => {
+                    Err(UserUpdateError::DuplicateDiscriminator)
+                }
+                UserRepositoryUpdateError::DuplicateEmail => Err(UserUpdateError::DuplicateEmail),
+                _ => Err(UserUpdateError::OtherError),
             },
             Ok(_) => Ok(()),
         }
