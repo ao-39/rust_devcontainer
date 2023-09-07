c41d553eb208a7747c0391b41550ba39c66e58d7
ao-39 <fukahireee@outlook.jp>
2023-09-07 21:52:42 +0900

    :sparkles: ユーザー情報更新のapiを追加した

--- app/api/src/user/mod.rs
+++ app/api/src/user/mod.rs
@@ -1,15 +1,17 @@
 mod delete;
 mod find_discriminator;
 mod register;
+mod update;
 use std::sync::Arc;
 
 use app_service_interface::user::IUserAppService;
 use axum::{
-    routing::{delete, get, post},
+    routing::{delete, get, patch, post},
     Extension, Router,
 };
 use delete::user_delete;
 use register::user_register;
+use update::user_update;
 
 use self::find_discriminator::find_by_discriminator;
 
@@ -21,5 +23,6 @@ where
         .route("/", post(user_register::<T>))
         .route("/:user_discriminator", get(find_by_discriminator::<T>))
         .route("/:user_discriminator", delete(user_delete::<T>))
+        .route("/:user_discriminator", patch(user_update::<T>))
         .layer(Extension(Arc::new(user_app_service)))
 }
---
+++ app/api/src/user/update.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use app_service_interface::object::email_address::EmailAddress;
+use app_service_interface::object::url::Url;
+use axum::extract::Path;
+use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
+
+use app_service_interface::object::{email_address, url, UserDiscriminator, UserName};
+use app_service_interface::user::{IUserAppService, UserRegisterError, UserUpdateOperator};
+use serde::{Deserialize, Serialize};
+
+pub async fn user_update<T>(
+    Extension(user_app_service): Extension<Arc<T>>,
+    Path(user_discriminator): Path<UserDiscriminator>,
+    Json(payload): Json<UserUpdateOperatorReqBody>,
+) -> Result<StatusCode, impl IntoResponse>
+where
+    T: IUserAppService + Send + Sync + 'static,
+{
+    let res = user_app_service
+        .update(user_discriminator, payload.into())
+        .await;
+    match res {
+        Ok(_) => Ok(StatusCode::NO_CONTENT),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UserUpdateOperatorReqBody {
+    Discriminator(UserDiscriminator),
+    Name(UserName),
+    Email(EmailAddress),
+    WebPage(Option<Url>),
+}
+
+impl From<UserUpdateOperatorReqBody> for UserUpdateOperator {
+    fn from(req_body: UserUpdateOperatorReqBody) -> Self {
+        match req_body {
+            UserUpdateOperatorReqBody::Discriminator(discriminator) => {
+                UserUpdateOperator::Discriminator(discriminator)
+            }
+            UserUpdateOperatorReqBody::Name(name) => UserUpdateOperator::Name(name),
+            UserUpdateOperatorReqBody::Email(email) => UserUpdateOperator::Email(email),
+            UserUpdateOperatorReqBody::WebPage(web_page) => UserUpdateOperator::WebPage(web_page),
+        }
+    }
+}
